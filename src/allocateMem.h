3cf115488b7390370c7bd89b8f0eb43199ba6859
elral <3263285+elral@users.noreply.github.com>
2026-03-27 10:25:48 +0100

    better format of of explanation in allocateMem.h

--- src/allocateMem.h
+++ src/allocateMem.h
@@ -3,6 +3,28 @@
 //
 // (C) MobiFlight Project 2022
 //
+// 
+// Single Object, avoids calling 'placement new' with nullptr
+//     void* mem = MF_ALLOC_TYPE(AccelStepper, 1);
+//     if (mem) {
+//         _stepper = new (mem) AccelStepper(AccelStepper::DRIVER, pin1, pin2);
+//     } else {
+//         // handle error
+//         return;
+//     }
+// 
+// Object array
+//     _encoders = static_cast<MFEncoder*>(MF_ALLOC_TYPE(MFEncoder, count));
+// 
+// uint8_t-Array
+//     _buffer = static_cast<uint8_t*>(MF_ALLOC_BYTES(count));
+// 
+// uint16_t-Array
+//     _values = static_cast<uint16_t*>(MF_ALLOC_TYPE(uint16_t, count));
+// 
+// uint32_t-Array
+//     _values = static_cast<uint32_t*>(MF_ALLOC_TYPE(uint32_t, count));
+// 
 
 #pragma once
 
@@ -17,27 +39,3 @@ void   ClearMemory();
 size_t GetAvailableMemory();
 
 // allocatemem.h
-
-/*
-Single Object, avoids calling 'placement new' with nullptr
-    void* mem = MF_ALLOC_TYPE(AccelStepper, 1);
-    if (mem) {
-        _stepper = new (mem) AccelStepper(AccelStepper::DRIVER, pin1, pin2);
-    } else {
-        // handle error
-        return;
-    }
-
-Object array
-    _encoders = static_cast<MFEncoder*>(MF_ALLOC_TYPE(MFEncoder, count));
-
-uint8_t-Array
-    _buffer = static_cast<uint8_t*>(MF_ALLOC_BYTES(count));
-
-uint16_t-Array
-    _values = static_cast<uint16_t*>(MF_ALLOC_TYPE(uint16_t, count));
-
-uint32_t-Array
-    _values = static_cast<uint32_t*>(MF_ALLOC_TYPE(uint32_t, count));
-
-*/
