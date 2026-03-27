6b0d910246fcf79217a50ca44838ab29de30fbc2
elral <3263285+elral@users.noreply.github.com>
2026-03-27 13:33:07 +0100

    better/extended explanation for device buffer usage

--- src/allocateMem.h
+++ src/allocateMem.h
@@ -4,26 +4,34 @@
 // (C) MobiFlight Project 2022
 //
 // 
-// Single Object, avoids calling 'placement new' with nullptr
-//     void* mem = MF_ALLOC_TYPE(AccelStepper, 1);
-//     if (mem) {
-//         _stepper = new (mem) AccelStepper(AccelStepper::DRIVER, pin1, pin2);
-//     } else {
-//         // handle error
-//         return;
-//     }
+// Single Object with constructor, avoids calling 'placement new' with nullptr
+//      void* mem = MF_ALLOC_TYPE(AccelStepper, 1);
+//      if (!mem) return false;
+//      _stepper = new (mem) AccelStepper(AccelStepper::DRIVER, pin1, pin2);
 // 
-// Object array
-//     _encoders = static_cast<MFEncoder*>(MF_ALLOC_TYPE(MFEncoder, count));
-// 
+// Object array with paramters in class, calls the constructor
+//      MFTestClass* _testClass = static_cast<MFTestClass*>(MF_ALLOC_TYPE(MFTestClass, count));
+//      if (!_testClass) return false;
+//      for (uint8_t i = 0; i < count; i++) {
+//          new (&_testClass[i]) MFTestClass(pin1, pin2);
+//      }
+//  and destructor if available
+//      for (uint8_t i = 0; i < count; i++) {
+//          _testClass[i].~MFTestClass();
+//      }
+//
+// Object array without constructor, paramters via _encoders.begin(x,y)
+//      _encoders = static_cast<MFEncoder*>(MF_ALLOC_TYPE(MFEncoder, count));
+//      if (!_encoders) return false;
+//
 // uint8_t-Array
-//     _buffer = static_cast<uint8_t*>(MF_ALLOC_BYTES(count));
+//      _buffer = static_cast<uint8_t*>(MF_ALLOC_BYTES(count));
 // 
 // uint16_t-Array
-//     _values = static_cast<uint16_t*>(MF_ALLOC_TYPE(uint16_t, count));
+//      _values = static_cast<uint16_t*>(MF_ALLOC_TYPE(uint16_t, count));
 // 
 // uint32_t-Array
-//     _values = static_cast<uint32_t*>(MF_ALLOC_TYPE(uint32_t, count));
+//      _values = static_cast<uint32_t*>(MF_ALLOC_TYPE(uint32_t, count));
 // 
 
 #pragma once
